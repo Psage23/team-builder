258511d3caa781e436e5d7a08c2ce5a8f66330c2
Psage23 <pamelasage3@gmail.com>
2019-11-13 18:08:22 -0700

    form created

---
+++ src/components/form.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+
+const Form = props => {
+    const [forms, setForms] = useState({
+        name: "",
+        location: "",
+        gender: "",
+        age:
+    });
+
+    const changeHandler = e => {
+        setForms({...forms, [e.target.name]: e.target.value})
+    }
+
+    const submitForm = e => {
+        e.preventDefault();
+        props.addNewForm(forms);
+        setForms({name: "", location: "", gender: "", age: })
+    };
+    return(
+        <form onSubmit={submitForm}>
+            <label htmlFor="name">Name</label>
+            <input name="name" id="name" type="text" placeholder="name" onChange={changeHandler} value={forms.name}/>
+
+            <label htmlFor="location">Location</label>
+            <input name="location" id="location" type="text" placeholder="location" onChange={changeHandler} value={forms.location}/>
+
+            <label htmlFor="gender">Gender</label>
+            <input name="gender" id="gender" type="text" placeholder="gender" onChange={changeHandler} value={forms.gender}/>
+
+            <label htmlFor="age">Age</label>
+            <input name="age" id="age" type="text" placeholder="age" onChange={changeHandler} value={forms.age}/>
+
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
+
+export default Form; 
---
+++ src/components/people.js
@@ -0,0 +1,22 @@
+
+export default [
+    {
+        name: "Bob",
+        location: "Texas",
+        gender: "Male",
+        age: 45
+    },
+    {
+        name: "Nancy",
+        location: "Iowa",
+        gender: "Female",
+        age: 33
+    },
+    {
+        name: "Stacy",
+        location: "Florida",
+        gender: "Female",
+        age: 22
+    }
+]
+
